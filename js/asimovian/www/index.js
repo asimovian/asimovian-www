af9cfbc02cb41f38ff405132ab83d56860cdd4e9
roylaurie <roy.laurie@asimovian.software>
2015-08-31 11:56:45 -0700

    index.js breakout

---
+++ js/asimovian/www/index.js
@@ -0,0 +1,26 @@
+/**
+ * @copyright 2015 Asimovian LLC
+ * @license MIT https://github.com/asimovian/www.asimovian.software/blob/master/LICENSE.txt
+ */
+'use strict';
+
+var gHexagonCanvas = (function() {
+  let hexagonCanvas = null;
+
+  WebFont.load({
+    custom: {
+      families: ['Quadaptor']
+    },
+    active: function() {
+        $(document).ready(function() {
+            let canvas = document.getElementById('hexagonCanvas');
+            canvas.width = document.body.clientWidth;
+            canvas.height = document.body.clientHeight;
+
+            hexagonCanvas = new HexagonCanvas(canvas, 30);
+            hexagonCanvas.draw();
+            console.log('Hexagonal canvas ready');
+        });
+    }
+  });
+})();
