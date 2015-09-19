696ca6fd3db05e893f2d38c1d69bb91df7d33f0d
roylaurie <roy.laurie@asimovian.software>
2015-09-18 17:30:10 -0700

    bootstrap

--- js/asimovian-www/app/index/App.js
+++ js/asimovian-www/app/index/App.js
@@ -1,7 +1,7 @@
 /**
  * @copyright 2015 Asimovian LLC
  * @license MIT https://github.com/asimovian/plur/blob/master/LICENSE.txt
- * @requires plur/PlurObject
+ * @requires plur/PlurObject plur/app/Application
  */
  'use strict';
 
